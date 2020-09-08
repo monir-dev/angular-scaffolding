0cc804cdb39a3b5644f0b95bd53573fa33830e56
Md Monir Hossain <echomonir@gmail.com>
2020-09-08 15:08:41 +0600

    Asp.net Core Api project created and added swagger

---
+++ ApiStarter/ApiStarter.Core/Controllers/UserController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using ApiStarter.Core.Data;
+using ApiStarter.Core.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiStarter.Core.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
+        private string _userId;
+
+        public UserController(ApplicationDbContext context, UserManager<IdentityUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+        }
+
+
+        [HttpGet]
+        public User GetUserById()
+        {
+            //_context.Users.Find(_userId);
+            var user = _context.Users.First();
+
+            var userModel = new User
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email,
+                PhoneNumber = user.PhoneNumber
+            };
+
+
+            return userModel;
+        }
+
+
+    }
+}
---
+++ ApiStarter/ApiStarter.Core/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace ApiStarter.Core.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+    }
+}
