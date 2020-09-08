5a31eecf94b585a0db1548c075b2f90bae472cd1
Md Monir Hossain <echomonir@gmail.com>
2020-09-08 16:45:28 +0600

    Working on user Api

--- ApiStarter/ApiStarter.WebApi/Controllers/UserController.cs
+++ ApiStarter/ApiStarter.WebApi/Controllers/UserController.cs
@@ -13,16 +13,51 @@ namespace ApiStarter.WebApi.Controllers
     {
         private readonly ApplicationDbContext _context;
 
-        public UserController(ApplicationDbContext context)
+        public UserController()
         {
-            _context = context;
+            _context = new ApplicationDbContext();
         }
 
-        public void GetById()
+        [HttpGet]
+        [Route("api/user/auth-user")]
+        public ApplicationUser GetAuthenticatedUser()
         {
             string userId = RequestContext.Principal.Identity.GetUserId();
+            if (userId == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.Unauthorized);
+            }
 
+            return _context.Users.Find(userId);
+        }
 
+        // GET api/user
+        public IEnumerable<ApplicationUser> Get()
+        {
+            var users = _context.Users.ToList();
+
+            return users;
+        }
+
+        // GET api/user/5
+        public ApplicationUser Get(string id)
+        {
+            return _context.Users.Find(id);
+        }
+
+        // POST api/user
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/user/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/user/5
+        public void Delete(int id)
+        {
         }
     }
 }
