da93916e74ac1ebb9491f54b7c9b431da3e5abee
Md Monir Hossain <echomonir@gmail.com>
2020-09-10 02:22:56 +0600

    User Endpoints creates

--- ApiStarter/ApiStarter.WebApi/Controllers/UserController.cs
+++ ApiStarter/ApiStarter.WebApi/Controllers/UserController.cs
@@ -3,12 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using ApiStarter.WebApi.Dtos;
 using ApiStarter.WebApi.Models;
 using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security.DataProtection;
 
 namespace ApiStarter.WebApi.Controllers
 {
+    [Authorize]
     public class UserController : ApiController
     {
         private readonly ApplicationDbContext _context;
@@ -31,28 +37,127 @@ namespace ApiStarter.WebApi.Controllers
             return _context.Users.Find(userId);
         }
 
+
         // GET api/user
-        public IEnumerable<ApplicationUser> Get()
+        public IEnumerable<UserDto> Get()
         {
-            var users = _context.Users.ToList();
+            var users = _context.Users
+                .Select(u => new UserDto
+                {
+                    Id = u.Id,
+                    Email = u.Email,
+                    UserName = u.UserName,
+                    PhoneNumber = u.PhoneNumber,
+                    Password = u.PasswordHash
+                })
+                .ToList();
 
             return users;
         }
 
         // GET api/user/5
-        public ApplicationUser Get(string id)
+        public UserDto Get(string id)
         {
-            return _context.Users.Find(id);
+            var user = _context.Users.Find(id);
+
+            if (user == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return  new UserDto
+            {
+                Id = user.Id,
+                Email = user.Email,
+                Password = user.PasswordHash,
+                PhoneNumber = user.PhoneNumber,
+                UserName = user.UserName
+            };
         }
 
         // POST api/user
-        public void Post([FromBody] string value)
+        public void Post([FromBody] UserDto userDto)
         {
+            //https://bitoftech.net/2015/01/21/asp-net-identity-2-with-asp-net-web-api-2-accounts-management/
+
+            try
+            {
+                //Create Identity User
+
+                var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+                var applicationUser = new ApplicationUser()
+                {
+                    UserName = userDto.UserName,
+                    Email = userDto.Email,
+                    EmailConfirmed = true,
+                    PhoneNumber = userDto.PhoneNumber
+                };
+
+                manager.Create(applicationUser, userDto.Password);
+
+
+                // Get Newly Created User
+                //var user = manager.FindByEmail(userDto.Email);
+
+                //return new UserDto
+                //{
+                //    Id = user.Id,
+                //    Email = user.Email,
+                //    Password = user.PasswordHash,
+                //    PhoneNumber = user.PhoneNumber,
+                //    UserName = user.UserName
+                //};
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+            
+            
         }
 
         // PUT api/user/5
-        public void Put(int id, [FromBody] string value)
+        public async Task PutAsync(string id, [FromBody] UserDto userDto)
         {
+            try
+            {
+                var userManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new ApplicationDbContext()));
+
+                // change Password
+                if (!string.IsNullOrEmpty(userDto.Password))
+                {
+                    var provider = new DpapiDataProtectionProvider("SampleAppName");
+                    userManager.UserTokenProvider = new DataProtectorTokenProvider<ApplicationUser>(
+                        provider.Create("SampleTokenName"));
+
+                    var token = await userManager.GeneratePasswordResetTokenAsync(id);
+                    await userManager.ResetPasswordAsync(id, token, userDto.Password);
+                }
+
+                // Update user informations
+                var user = await userManager.FindByIdAsync(id);
+                user.PhoneNumber = userDto.PhoneNumber;
+                user.Email = userDto.Email;
+                user.UserName = userDto.Email;
+                await userManager.UpdateAsync(user);
+
+
+                //return new UserDto
+                //{
+                //    Id = user.Id,
+                //    Email = user.Email,
+                //    Password = user.PasswordHash,
+                //    PhoneNumber = user.PhoneNumber,
+                //    UserName = user.UserName
+                //};
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
         }
 
         // DELETE api/user/5
